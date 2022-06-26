2327308635764ab15f9447d6ad773d8723980b46
Shubhechchha Dahal <dahal.shubhechchha@gmail.com>
2022-06-26 15:20:33 +0200

    intial grid maangement

--- src/components/candidates/candidadeList.js
+++ src/components/candidates/candidadeList.js
@@ -1,10 +1,11 @@
 import CandidateItem from './candidateItem';
-import classes from './candidateList.module.css';
-
+import {Grid} from '@material-ui/core';
 
 function CandidateList(props) {
-    return <ul className={classes.list}>
+    return (
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {props.candidates.map ( (candidate) => ( 
+          <Grid item md={3}>
             <CandidateItem 
                 key={candidate.id} 
                 id={candidate.id} 
@@ -12,8 +13,10 @@ function CandidateList(props) {
                 name ={candidate.name}
                 description = {candidate.description}
             />
-           ))}
-    </ul>
+          </Grid> 
+         ))} 
+      </Grid> 
+    );  
 }
 
 export default CandidateList;
--- src/components/candidates/candidateItem.js
+++ src/components/candidates/candidateItem.js
@@ -1,17 +1,19 @@
-import classes from './candidateItem.module.css';
+ import classes from './candidateItem.module.css';
 
 function CandidateItem(props) {
+
+    function handleVoter() {}
+
     return <li className={classes.item}>
         <div className={classes.image}>
             <img src={props.image} alt={props.name} />
         </div>
-
         <div className={classes.content}>
             <h3>{props.name}</h3>
             <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-            <button>VOTE</button>
+            <button onClick={handleVoter}>VOTE</button>
         </div>
     </li>
 }
